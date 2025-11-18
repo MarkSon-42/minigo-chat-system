a3a70e6ba2cd2139ec9b6a0dfb925257a1b01030
MarkSon-42 <coffeefirst5811@gmail.com>
2025-11-18 17:26:12 +0900

    feat(storage): define storage struct and functions

--- proxy/storage.go
+++ proxy/storage.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+	"os"
+	"sync"
+)
+
+type Storage struct {
+	mu       sync.Mutex
+	file     *os.File
+	filepath string
+	enabled  bool
+}
+
+// NewStorage creates a new storage instance
+func NewStorage(filepath string) (*Storage, error) {
+	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("[Storage] Initialized: %s", filepath)
+
+	return &Storage{
+		file:     file,
+		filepath: filepath,
+		enabled:  true,
+	}, nil
+}
