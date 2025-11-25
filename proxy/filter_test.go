f6e42e13a692b512a14aada0b1eb6d0308888939
MarkSon-42 <coffeefirst5811@gmail.com>
2025-11-25 16:27:15 +0900

    test(proxy): tide test codes and rewrite filter test code

---
+++ proxy/filter_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+// checkmessage block test
+func TestCheckMessage_Block(t *testing.T) {
+	filter := NewFilter()
+	msg := &Message{
+		Type:    "message",
+		Content: "This contains badword in text",
+	}
+
+	allowed, _ := filter.CheckMessage(msg)
+	if allowed {
+		t.Error("Expected message to be blocked")
+	}
+}
