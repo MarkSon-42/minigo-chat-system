e5931d74c713c61e2f49a4389a2be52bfb85bc7f
MarkSon-42 <coffeefirst5811@gmail.com>
2025-11-25 17:44:25 +0900

    test(proxy): filter test code .. concurrency testing

--- proxy/filter_test.go
+++ proxy/filter_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 )
 
-// checkmessage block test
 func TestCheckMessage_Block(t *testing.T) {
 	filter := NewFilter()
 	msg := &Message{
@@ -16,4 +15,39 @@ func TestCheckMessage_Block(t *testing.T) {
 	if allowed {
 		t.Error("Expected message to be blocked")
 	}
+}
+func TestCheckMessage_Repalce(t *testing.T) {
+
+}
+
+func TestCheckMessage_EmptyContent(t *testing.T) {
+
+}
+
+func TestAddRule(t *testing.T) {
+
+}
+
+func TestRemoveRule(t *testing.T) {
+
+}
+
+func TestUpdateRule(t *testing.T) {
+
+}
+
+func TestConcurrentAccess(t *testing.T) {
+	filter := NewFilter()
+	done := make(chan bool)
+
+	// concurrent read
+	for i := 0; i < 10; i++ {
+		go func() {
+			msg := &Message{Type: "message", Content: "test"}
+			filter.CheckMessage(msg)
+			done <- true
+		}()
+	}
+
+	// concurrent write
 }
