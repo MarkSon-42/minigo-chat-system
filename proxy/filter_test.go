a9fb5d7cd92effa39e90c476674340b38a808c43
MarkSon-42 <coffeefirst5811@gmail.com>
2025-11-27 16:32:46 +0900

    fix(proxy): password block logic edit

--- proxy/filter_test.go
+++ proxy/filter_test.go
@@ -13,3 +13,18 @@ func TestCheckMessage_Block(t *testing.T) {
 		t.Error("Expected message to be blocked")
 	}
 }
+
+func TestCheckMessage_PasswordBlock(t *testing.T) {
+	filter := NewFilter()
+	msg := &Message{
+		Type:    "message",
+		Content: "My password is 12345",
+	}
+	allowed, result := filter.CheckMessage(msg)
+	if !allowed {
+		t.Error("Expected message to be blocked")
+	}
+	if result == nil {
+		t.Fatal("Expected result to be nil when blocked")
+	}
+}
