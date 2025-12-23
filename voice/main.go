487f850e1e77a86d2e3ed49b33f04d0d99171d8f
MarkSon-42 <coffeefirst5811@gmail.com>
2025-12-23 16:32:56 +0900

    feat(voice): main var group created

--- voice/main.go
+++ voice/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/pion/webrtc/v4"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true // CORS 허용..했는데 프로덕션에선 제한 해야함.
+		},
+	}
+
+	peersMutex sync.RWMutex
+	peers      = make(map[string]*webrtc.PeerConnection) // username -< PeerConnection
+)
 
 func main() {
 	log.Println("Voice SFU Server starting...")
-	// TODO: WebSocket signaling server
-	// TODO: SFU initialization
+
+	http.HandleFunc("/ws", handleWebSocket)
+
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		log.Fatal("Server failed:", err)
+	}
 }
